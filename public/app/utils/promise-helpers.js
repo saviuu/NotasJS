200b8ac0e61cd62831d05972f67fd6a56dd64b8f
Sávio Bertoldi <savio.b09@gmail.com>
2023-05-24 18:49:46 -0300

    Update promise-helpers.js

--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -1,10 +1,11 @@
 export const handleStatus = res => 
     res.ok ? res.json() : Promise.reject(res.statusText);
 
-    export const log = param => {
+    
+export const log = param => {
         console.log(param);
         return param;
-    }    
+}    
 
 
     
