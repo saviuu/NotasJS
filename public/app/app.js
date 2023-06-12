9e4f3bb00e27b1918f14b009b17e06013757dc1a
Sávio Bertoldi <savio.b09@gmail.com>
2023-06-12 18:21:08 -0300

    Ajustes nas promises e events

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,24 @@
-import { log } from './utils/promise-helpers.js';
+import { log, timeoutPromise, delay, retry } from './utils/promise-helpers.js';
 import './utils/array.helpers.js';
 import { notasService } from './nota/nota.service.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js'
+
+const operations = pipe(
+        partialize(takeUntil, 3),
+        partialize(debounceTime, 500)
+);
+
+
+const action = operations(() => 
+        retry(3, 3000, () => timeoutPromise(200, notasService.sumItems('2143'))) 
+        .then(delay(1000))
+        .then(total => EventEmitter.emit('itensTotalizados', total))
+        .catch(console.log)
+);
+
 
 document
 .querySelector('#myButton')
-.onclick = () => 
-        notasService
-        .sumItems('2143')
-        .then(console.log)
-        .catch(console.log);
+.onclick = action;
 
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -6,3 +6,14 @@ export const compose = (...functions) => value =>
 
 export const pipe = (...functions) => value => 
     functions.reduce((previousValue, fn) => fn(previousValue), value)
+
+export const takeUntil = (times, fn) => 
+     () => times-- > 0 && fn();
+    
+export const debounceTime = (milliseconds, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer);
+        timer = setTimeout(fn, milliseconds);
+    }
+}
